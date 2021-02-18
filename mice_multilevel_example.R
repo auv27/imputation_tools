e7668e3f1f3ea757e693433bf0b617a29535c023
Alicia Vallorani <auv27@psu.edu>
2021-02-18 13:52:15 -0500

    Update mice_multilevel_example.R
    adjusting model

--- mice_multilevel_example.R
+++ mice_multilevel_example.R
@@ -1,33 +1,33 @@
 if (!("pacman" %in% installed.packages()[,])) {
   install.packages("pacman")
 }
-pacman::p_load(tidyverse, mice, miceadds)
+pacman::p_load(tidyverse, mice, miceadds, lme4, lmerTest, optimx)
 
 # data.frame with missing data example
 df <- data.frame(id = c(1, 2, 3, 4, 5),
                  x = c(46, NA, 23, 24, 47),
                  y1 = c(16, 28, 36, NA, 42),
                  y2 = c(33, 45, NA, 16, 2),
-                 y3 = c(34, 18, 39, 4, NA)) %>%
+                 y3 = c(34, 18, 39, 4, NA),
+                 y4 = c(NA, 26, 47, 19, 24)) %>%
   # you will want your data in a long format if it isn't already
-  pivot_longer(., cols = 3:5, names_to = "type", values_to = "value") %>%
+  pivot_longer(., cols = 3:6, names_to = "type", values_to = "value") %>%
   # if your model will have interactions you should include those in the dataset for imputation
   mutate(x = as.numeric(scale(x, center = TRUE, scale = FALSE)),
          type = as.numeric(gsub("[^0-9.-]", "", type))) %>%
   rowwise() %>%
-  mutate(x.y = x*type,
-         type = as.factor(type)) %>%
+  mutate(x.y = x*type) %>%
   as.data.frame() 
 
 # set id as a grouping variable 
 ini <- mice(df, maxit = 0, pri = FALSE)
 pred <- ini$pred
-pred[,c("id")] -2
+pred[, "id"] <- -2
 
 # set multilevel imputation method
 meth <- ini$meth
-meth[c("value", "x", "x.y")] <- "2l.norm"
-#meth[c("x", "y", "x.y")] <- "pmm"
+meth[c("value")] <- "2l.norm"
+meth[c("x", "x.y")] <- "pmm"
 meth[c("type")] <- ""
 
 # imputing missing values getting 25 datasets, 50 dataframes within each 
@@ -40,5 +40,6 @@ df_imputed2 <- lapply(1:25, function(i) complete(df_imputed, action = i))
 df_imputed_complete <- datalist2mids(df_imputed2) 
 
 # example model
-m <- with(df_imputed, lm(z ~ x*y))
-summary(est <- pool(m))
+m <- with(df_imputed, lmer(value ~ x*type + (1 | id))) 
+pool(m)
+summary(est)
